ba6a854d9a2938c83776b09f2d661963a80a9d6a
Pedro Del Col <pedroavgdelcol@gmail.com>
2025-04-17 13:37:59 -0300

    description adicionada e card ajustado para um grid

--- reusable_component/src/app/page.tsx
+++ reusable_component/src/app/page.tsx
@@ -3,12 +3,113 @@ import DestinationCard from '@/components/destinationCard/DestinationCard';
 export default function Home() {
     return (
         <main>
-            <DestinationCard
-                image="/images/paris_destination_card_image.avif"
-                name="Paris"
-                price="R$ 3.200"
-                evaluation={5}
-            />
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-3">
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+                <DestinationCard
+                    image="/images/paris_destination_card_image.avif"
+                    name="Paris"
+                    price="R$ 3.200"
+                    evaluation={5}
+                    description='"Paris é a cidade dos sonhos — um palco de romance, arte e história, onde cada rua revela um charme irresistível e experiências inesquecíveis!"'
+                />
+            </div>
         </main>
     );
 }
--- reusable_component/src/components/destinationCard/DestinationCard.tsx
+++ reusable_component/src/components/destinationCard/DestinationCard.tsx
@@ -6,21 +6,30 @@ interface DestinationCardProps {
     name: string;
     price: string;
     evaluation: number;
+    description: string;
 }
 
-const DestinationCard = ({image, name, price, evaluation}: DestinationCardProps) => {
+const DestinationCard = ({image, name, price, evaluation, description}: DestinationCardProps) => {
     return (
-        <div className="relative w-[300] h-[350] rounded-xl overflow-hidden shadow-lg transition-all duration-300 ease-out hover:-translate-y-1 hover:shadow-2xl">
-            <Image
-                src={image}
-                alt={`Image of ${name}`}
-                layout="fill"
-                objectFit="cover"
-                className="brightness-75"/>
-            <div className="absolute bottom-0 left-0 p-4 text-white z-10">
-                <h2 className="text-2xl font-semibold drop-shadow-md">{name}</h2>
-                <p className="text-lg drop-shadow-sm">{price}</p>
-                <p className="text-lg drop-shadow-sm">{evaluation} ★</p>
+        <div className="relative group w-full h-[350] rounded-xl overflow-hidden shadow-lg transition-all duration-500 ease-out hover:-translate-y-2 hover:shadow-2xl">
+                <Image
+                    src={image}
+                    alt={`Image of ${name}`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="brightness-75"/>
+            <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50 transition">
+            </div>
+            <div className="absolute inset-0 flex flex-col justify-end p-4 text-white z-10">
+  
+                <div className="transition-all duration-500 ease-in-out transform translate-y-1 group-hover:translate-y-0">
+                    <h2 className="text-2xl font-semibold drop-shadow-md">{name}</h2>
+                    <p className="text-lg drop-shadow-sm">{price}</p>
+                    <p className="text-lg drop-shadow-sm">{evaluation} ★</p>
+                </div>
+                <p className="text-sm italic opacity-0 max-h-0 overflow-hidden transform translate-y-2 group-hover:opacity-100 group-hover:max-h-40 group-hover:translate-y-0 transition-all duration-500 ease-in-out">
+                    {description}
+                </p>
             </div>
         </div>
     );
